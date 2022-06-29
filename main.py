79e6f177e50e96b900ee633db536423518f5d43c
HenriqueCorsi <106001465+HenriqueCorsi@users.noreply.github.com>
2022-06-29 14:10:31 -0300

    Principal
    Responsável por executar o sistema.

---
+++ main.py
@@ -0,0 +1,23 @@
+from validador_cpf import *
+from validador_cnpj import *
+from menu import *
+
+
+menu()
+select_user = int(input())
+print('-------------------------------')
+if select_user == 1:  # CPF
+    cpf = input('Digite o CPF que gostaria de verificar: ')
+    validador_cpf(cpf)
+elif select_user == 2:  # CNPJ
+    cnpj = input('Digite o CNPJ que gostaria de verificar: ')
+    validador_cnpj(cnpj)
+elif select_user == 3:  # TELEFONE
+    pass
+elif select_user == 4:  # CEP
+    pass
+elif select_user == 5:
+    print('Até logo...')
+else:
+    print('Opção Inválida')
+
