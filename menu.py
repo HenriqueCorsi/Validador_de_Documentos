5c7f29f204fdca596d3cca31e27f591d3a340120
HenriqueCorsi <106001465+HenriqueCorsi@users.noreply.github.com>
2022-06-29 14:14:16 -0300

    bbl menu
    Construção de um menu para interação com o usuário.

---
+++ menu.py
@@ -0,0 +1,10 @@
+def menu():
+    print('-------------------------------')
+    print('     VALIDADOR DE DOCUMENTOS   ')
+    print('-------------------------------')
+    menu = ['[1] -> CPF',
+            '[2] -> CNPJ',
+            '[5] -> Sair']
+    for x in menu:
+        print(x)
+    print('-------------------------------')
